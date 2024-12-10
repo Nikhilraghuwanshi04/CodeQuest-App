d2738c0c400feae97b1e00c71785140d5254c046
Nikhilraghuwanshi04 <Nikhilraghuwanshi04@gmail.com>
2024-12-10 23:00:57 +0530

    files are added

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
- const userschema=mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    about:{type:String},
-    tags:{type:[String]},
-    joinedon:{type:Date,default:Date.now}
- })
 
- export default mongoose.model("User",userschema)
+const UserSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    about: { type: String },
+    tags: { type: [String] },
+    joinedon: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("User", UserSchema);
